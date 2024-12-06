bc43007e59f456338aed35e0e104053977a51602
xuruifan <707185547@qq.com>
2024-12-06 09:34:06 +0800

    Update jquery.greedy-navigation.js

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -23,7 +23,7 @@ function updateNav() {
     breaks.push($vlinks.width());
 
     // Move item to the hidden list
-    $vlinks.children('*:not(.tutorial__menu-item)').last().prependTo($hlinks);
+    $vlinks.children('*:not(.masthead__menu-item--lg)').last().prependTo($hlinks);
 
     // Show the dropdown btn
     if($btn.hasClass('hidden')) {
